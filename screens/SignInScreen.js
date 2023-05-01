05f8d39c5a3295586b1fe005f8333219f60abbb1
Robin Blondin <robin.blondin@protonmail.com>
2023-05-01 19:08:42 +0200

    Designed a layout for the Sign in screen

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -1,14 +1,91 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, JSX, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 
 const SignInScreen = () => {
   return (
-    <View>
-      <Text>SignInScreen</Text>
+    <View style={styles.outerContainer}>
+      <Image
+        style={styles.logo}
+        source={require('/home/robin/repos/chatup/images/logo.png')} />
+      <View>
+        <Text style={styles.text}>Login to continue</Text>
+      </View>
+      <View style={styles.innerContainer}>
+        <TextInput 
+          style={styles.textInput}
+          placeholder='Enter email adress'
+          placeholderTextColor={'#87888c'} />
+        <TextInput 
+          style={styles.textInput}
+          placeholder='Enter password'
+          placeholderTextColor={'#87888c'} />
+      </View>
+      
+        <TouchableOpacity style={styles.buttonContainer}>
+          <View>
+            <Text style={styles.buttonContainer}>Sign in</Text>
+          </View>
+        </TouchableOpacity>
+      
+      <View>
+        <TouchableOpacity>
+            <Text style={styles.text}>Create a new account</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
 
 export default SignInScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  outerContainer: {
+    flex: 1,
+    backgroundColor: '#20232b',
+    te: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+
+  innerContainer: {
+    marginTop: 10,
+    alignSelf: 'stretch',
+    gap: 8,
+
+  },
+
+  text: {
+    color: '#87888c',
+    alignItems: 'center',
+    alignSelf: 'stretch',
+    padding: 12,
+  },
+
+  textInput: {
+
+    backgroundColor: '#16171b',
+    paddingVertical: 8,
+    paddingLeft: 12,
+    alignItems: 'stretch',
+    borderRadius: 10,
+  },
+
+  buttonContainer: {
+
+    alignSelf: 'stretch',
+    marginTop: 8,
+    backgroundColor: '#5dbe74',
+    alignItems: 'center',
+    borderRadius: 10,
+    padding: 2,
+    paddingBottom: 8,
+  },
+
+  logo: {
+
+    width: 300,
+    height: 191,
+
+  },
+});
