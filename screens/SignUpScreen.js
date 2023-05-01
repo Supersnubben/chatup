1f756bbd3ae8b0327125f02f5d01b41b987b7d13
Robin Blondin <robin.blondin@protonmail.com>
2023-05-02 00:10:35 +0200

    Added functions that checks if every input is valid
    All fields needs to be filled in with valid data to be able to
    finish the registration process. If not the user will get an alert
    showing what's wrong.

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -1,56 +1,92 @@
-import { Image, JSX, TouchableOpacity, TextInput, StyleSheet, Text, View, KeyboardAvoidingView } from 'react-native'
-import styles from '../styles/styles.js'
-import React from 'react'
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity, TextInput, KeyboardAvoidingView, Alert } from 'react-native';
+import styles from '../styles/styles.js';
 
 const SignUpScreen = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleSignUp = () => {
+    if (!name || !email || !password || !confirmPassword) {
+      Alert.alert('Please fill all fields.');
+      return;
+    }
+
+    if (!validateEmail(email)) {
+      Alert.alert('Please enter a valid email address.');
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      Alert.alert('Passwords do not match.');
+      return;
+    }
+
+    // continue with signup process
+    // ...
+  };
+
+  const validateEmail = (email) => {
+    // regex pattern for email validation
+    const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return pattern.test(email);
+  };
+
   return (
     <KeyboardAvoidingView 
       style={styles.shellContainer}
       behavior='padding'>
-        <TouchableOpacity>
-              <Image style={styles.backButton} source={require('../images/backbutton.png')} />
-        </TouchableOpacity>
-    <View style={styles.outerContainer}>
-
       <TouchableOpacity>
-        <View style={styles.addImage}>
-          <Text>Add image</Text>
-        </View>
+        <Image style={styles.backButton} source={require('../images/backbutton.png')} />
       </TouchableOpacity>
-        
-        
-
-      <View style={styles.innerContainer}>
-        <TextInput 
-          style={styles.textInput}
-          placeholder='Enter full name'
-          placeholderTextColor={'#87888c'} />
-        <TextInput 
-          style={styles.textInput}
-          placeholder='Enter email'
-          placeholderTextColor={'#87888c'} />
-          <TextInput 
-          style={styles.textInput}
-          placeholder='Enter password'
-          placeholderTextColor={'#87888c'}
-          secureTextEntry
-          />
-        <TextInput 
-          style={styles.textInput}
-          placeholder='Confirm password'
-          placeholderTextColor={'#87888c'}
-          secureTextEntry />
-      </View>
-      
-        <TouchableOpacity style={styles.buttonContainer}>
-          <View>
-            <Text style={styles.buttonContainer}>Register account</Text>
+      <View style={styles.outerContainer}>
+        <TouchableOpacity>
+          <View style={styles.addImage}>
+            <Text>Add image</Text>
           </View>
         </TouchableOpacity>
-      
+        <View style={styles.innerContainer}>
+          <TextInput 
+            style={styles.textInput}
+            placeholder='Enter full name'
+            placeholderTextColor={'#87888c'}
+            value={name}
+            onChangeText={text => setName(text)} />
+          <TextInput 
+            style={styles.textInput}
+            placeholder='Enter email'
+            placeholderTextColor={'#87888c'}
+            value={email}
+            onChangeText={text => setEmail(text)} />
+          <TextInput 
+            style={styles.textInput}
+            placeholder='Enter password'
+            placeholderTextColor={'#87888c'}
+            secureTextEntry
+            value={password}
+            onChangeText={text => setPassword(text)}
+          />
+          <TextInput 
+            style={styles.textInput}
+            placeholder='Confirm password'
+            placeholderTextColor={'#87888c'}
+            secureTextEntry
+            value={confirmPassword}
+            onChangeText={text => setConfirmPassword(text)}
+          />
+        </View>
+        <TouchableOpacity 
+          style={styles.buttonContainer}
+          onPress={handleSignUp}>
+          <View>
+            <Text style={styles.buttonText}>Register account</Text>
+          </View>
+        </TouchableOpacity>
       </View>
-      </KeyboardAvoidingView>
-  )
-}
+    </KeyboardAvoidingView>
+  );
+};
 
-export default SignUpScreen
+export default SignUpScreen;
