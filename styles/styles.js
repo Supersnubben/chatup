a2b7e909dad8adf06daa80dd53b69f32fd17fcbf
Robin Blondin <robin.blondin@protonmail.com>
2023-05-02 23:30:25 +0200

    Designed the layout of the HomeScreen

--- styles/styles.js
+++ styles/styles.js
@@ -1,6 +1,7 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
+
   outerContainer: {
     flex: 1,
     backgroundColor: '#9af3f3',
@@ -9,30 +10,47 @@ export default StyleSheet.create({
     padding: 20,
   },
 
+  outerContainerDark: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'stretch',
+    backgroundColor: '#25b8bf',
+  },
+
+  shellContainer: {
+    flex: 1,
+    backgroundColor: '#9af3f3',
+    padding: 30,
+  },
+
   innerContainer: {
     marginTop: 10,
     alignSelf: 'stretch',
     gap: 8,
-
   },
 
-  text: {
-    color: '#87888c',
+  innerContainerHome: {
+    flex: 6,
+    backgroundColor: '#9af3f3',
+    borderRadius: 50,
+  },
+
+  nameContainerHome: {
+    marginTop: 20,
+    width: 180,
+    height: 25,
     alignItems: 'center',
-    alignSelf: 'stretch',
-    marginTop: 12,
+    justifyContent: 'center',
   },
 
-  textInput: {
-
-    backgroundColor: '#EDFFFF',
-    paddingVertical: 8,
-    paddingLeft: 12,
-    alignItems: 'stretch',
-    borderRadius: 10,
-    borderWidth: 2,
-    borderColor: '#25b8bf'
+  headerHome: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignContent: 'center',
+    justifyContent: 'space-between',
   },
+
 
   buttonContainer: {
     color: 'white',
@@ -46,11 +64,40 @@ export default StyleSheet.create({
     padding: 10,
   },
 
-  logo: {
+  text: {
+    color: '#87888c',
+    alignItems: 'center',
+    alignSelf: 'stretch',
+    marginTop: 12,
+  },
 
+  textInput: {
+    backgroundColor: '#EDFFFF',
+    paddingVertical: 8,
+    paddingLeft: 12,
+    alignItems: 'stretch',
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: '#25b8bf'
+  },
+
+  logo: {
     width: 300,
     height: 191,
+  },
 
+  logout: {
+    marginTop: 20,
+    marginRight: 30,
+    width: 25,
+    height: 25
+  },
+
+  add: {
+    width: 175,
+    height: 175,
+    marginLeft: '35%',
+    marginBottom: 100,
   },
 
   addImage: {
@@ -64,7 +111,6 @@ export default StyleSheet.create({
     backgroundColor: '#EDFFFF',
     borderColor: '#25b8bf',
     borderWidth: 2,
-
   },
 
   backButton: {
@@ -74,12 +120,6 @@ export default StyleSheet.create({
 
   },
 
-  shellContainer: {
-    flex: 1,
-    backgroundColor: '#9af3f3',
-    padding: 30,
-  },
-
   profileImage: {
     marginBottom: 50,
     width: 150,
@@ -87,6 +127,16 @@ export default StyleSheet.create({
     borderWidth: 2,
     borderColor: '#25b8bf',
     borderRadius: 150 / 2,
+  },
+
+  profileImageHome: {
+    marginTop: 20,
+    marginLeft: 20,
+    width: 50,
+    height: 50,
+    borderWidth: 2,
+    borderColor: '#9af3f3',
+    borderRadius: 50 / 2,
   },
 
 });
