0bbf4109b8160ae914b59737248a6e7e88e65daa
Robin Blondin <robin.blondin@protonmail.com>
2023-05-02 13:23:19 +0200

    Added imagepicker function
    lets the user choose an image from the device library as profile picture

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import { ToastAndroid } from 'react-native';
-import { StyleSheet, Text, View, Image, TouchableOpacity, TextInput, KeyboardAvoidingView, Alert } from 'react-native';
+import { ToastAndroid, Image, TouchableOpacity, TextInput, KeyboardAvoidingView } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import { StyleSheet, Text, View } from 'react-native';
 import styles from '../styles/styles.js';
 
 const SignUpScreen = ({ navigation }) => {
@@ -8,6 +9,7 @@ const SignUpScreen = ({ navigation }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+  const [image, setImage] = useState(null);
 
   const handleSignUp = () => {
     if (!name || !email || !password || !confirmPassword) {
@@ -24,6 +26,12 @@ const SignUpScreen = ({ navigation }) => {
       ToastAndroid.show('Passwords do not match.', ToastAndroid.SHORT);
       return;
     }
+
+    if (!image) {
+      ToastAndroid.show('Please select a profile picture.', ToastAndroid.SHORT);
+      return;
+    }
+
     ToastAndroid.show('All fields are filled in correctly.', ToastAndroid.SHORT);
     // continue with signup process
   };
@@ -38,6 +46,19 @@ const SignUpScreen = ({ navigation }) => {
     navigation.goBack();
   };
 
+  const handleSelectImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+    });
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
   return (
     <KeyboardAvoidingView
       style={styles.shellContainer}
@@ -47,10 +68,14 @@ const SignUpScreen = ({ navigation }) => {
           style={styles.backButton} source={require('../images/backbutton.png')} />
       </TouchableOpacity>
       <View style={styles.outerContainer}>
-        <TouchableOpacity>
-          <View style={styles.addImage}>
-            <Text style={{ color: '#87888c' }}>Add image</Text>
-          </View>
+        <TouchableOpacity onPress={handleSelectImage}>
+          {image ? (
+            <Image style={styles.profileImage} source={{ uri: image }} />
+          ) : (
+            <View style={styles.addImage}>
+              <Text style={{ color: '#87888c' }}>Add image</Text>
+            </View>
+          )}
         </TouchableOpacity>
         <View style={styles.innerContainer}>
           <TextInput
--- styles/styles.js
+++ styles/styles.js
@@ -1,82 +1,92 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-    outerContainer: {
-      flex: 1,
-      backgroundColor: '#9af3f3',
-      alignItems: 'center',
-      justifyContent: 'center',
-      padding: 20,
-    },
-  
-    innerContainer: {
-      marginTop: 10,
-      alignSelf: 'stretch',
-      gap: 8,
-  
-    },
-  
-    text: {
-      color: '#87888c',
-      alignItems: 'center',
-      alignSelf: 'stretch',
-      marginTop: 12,
-    },
-  
-    textInput: {
-  
-      backgroundColor: '#EDFFFF',
-      paddingVertical: 8,
-      paddingLeft: 12,
-      alignItems: 'stretch',
-      borderRadius: 10,
-      borderWidth: 2,
-      borderColor: '#25b8bf'
-    },
-  
-    buttonContainer: {
-      color: 'white',
-      alignSelf: 'center',
-      justifyContent: 'center',
-      width: '100%',
-      height: 45,
-      backgroundColor: '#25b8bf',
-      alignItems: 'center',
-      borderRadius: 10,
-      padding: 10,
-    },
-  
-    logo: {
-  
-      width: 300,
-      height: 191,
-  
-    },
+  outerContainer: {
+    flex: 1,
+    backgroundColor: '#9af3f3',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
 
-    addImage: {
-        color: '#87888c',
-        marginBottom: 50,
-        width: 150,
-        height: 150,
-        borderRadius: 150/2,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#EDFFFF',
-        borderColor: '#25b8bf',
-        borderWidth: 2,
+  innerContainer: {
+    marginTop: 10,
+    alignSelf: 'stretch',
+    gap: 8,
 
-    },
+  },
 
-    backButton: {
-        width: 25,
-        height: 25,
-        marginTop: 20,
+  text: {
+    color: '#87888c',
+    alignItems: 'center',
+    alignSelf: 'stretch',
+    marginTop: 12,
+  },
 
-    },
+  textInput: {
 
-      shellContainer: {
-        flex: 1,
-        backgroundColor: '#9af3f3',
-        padding: 30,
-      },
-  });
+    backgroundColor: '#EDFFFF',
+    paddingVertical: 8,
+    paddingLeft: 12,
+    alignItems: 'stretch',
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: '#25b8bf'
+  },
+
+  buttonContainer: {
+    color: 'white',
+    alignSelf: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    height: 45,
+    backgroundColor: '#25b8bf',
+    alignItems: 'center',
+    borderRadius: 10,
+    padding: 10,
+  },
+
+  logo: {
+
+    width: 300,
+    height: 191,
+
+  },
+
+  addImage: {
+    color: '#87888c',
+    marginBottom: 50,
+    width: 150,
+    height: 150,
+    borderRadius: 150 / 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#EDFFFF',
+    borderColor: '#25b8bf',
+    borderWidth: 2,
+
+  },
+
+  backButton: {
+    width: 25,
+    height: 25,
+    marginTop: 20,
+
+  },
+
+  shellContainer: {
+    flex: 1,
+    backgroundColor: '#9af3f3',
+    padding: 30,
+  },
+
+  profileImage: {
+    marginBottom: 50,
+    width: 150,
+    height: 150,
+    borderWidth: 2,
+    borderColor: '#25b8bf',
+    borderRadius: 150 / 2,
+  },
+
+});
