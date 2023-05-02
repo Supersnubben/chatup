591b8306648f6da37cf85af4a2e2c69632e7dbb2
Robin Blondin <robin.blondin@protonmail.com>
2023-05-02 21:17:04 +0200

    Added function to let registered users sign in.
    When signed in users get redirected to HomeScreen.

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -1,9 +1,40 @@
-import { Image, JSX, StyleSheet, Text, TextInput, TouchableOpacity, View, KeyboardAvoidingView } from 'react-native'
+import { ToastAndroid, Image, JSX, StyleSheet, Text, TextInput, TouchableOpacity, View, KeyboardAvoidingView } from 'react-native'
 import styles from '../styles/styles.js'
-import React from 'react'
+import React, { useState } from 'react';
 import SignUpScreen from './SignUpScreen';
+import HomeScreen from './HomeScreen'
+import { signInWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { auth } from '../firebase.js';
 
 const SignInScreen = ({ navigation }) => {
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleSignIn = () => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in successfully
+        const user = userCredential.user;
+        navigation.navigate('HomeScreen');
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // Show error message
+        if (errorCode === 'auth/user-not-found') {
+          ToastAndroid.show('User not found. Please register.', ToastAndroid.SHORT);
+        }
+        else if (errorCode === 'auth/wrong-password') {
+          ToastAndroid.show('Incorrect password. Please try again.', ToastAndroid.SHORT);
+        }
+        else if (errorCode === 'auth/invalid-email') {
+          ToastAndroid.show('Invalid email format. Please try again.', ToastAndroid.SHORT);
+        }
+        else {
+          ToastAndroid.show(errorMessage, ToastAndroid.SHORT);
+        }
+      });
+  };
 
   const handlePress = () => {
     navigation.navigate('SignUpScreen');
@@ -25,13 +56,17 @@ const SignInScreen = ({ navigation }) => {
           <TextInput
             style={styles.textInput}
             placeholder='Enter email adress'
-            placeholderTextColor={'#87888c'} />
+            placeholderTextColor={'#87888c'}
+            value={email}
+            onChangeText={text => setEmail(text)} />
           <TextInput
             style={styles.textInput}
             placeholder='Enter password'
             placeholderTextColor={'#87888c'}
-            secureTextEntry />
-          <TouchableOpacity style={styles.buttonContainer}>
+            secureTextEntry
+            value={password}
+            onChangeText={text => setPassword(text)} />
+          <TouchableOpacity onPress={handleSignIn} style={styles.buttonContainer}>
             <View>
               <Text style={styles.buttonContainer}>Sign in</Text>
             </View>
