3b65b860707d68b8c865ae158a4a29b3c9df2f50
Robin Blondin <robin.blondin@protonmail.com>
2023-05-04 22:48:39 +0200

    Setup firestore. Add method to encode img to string
    Add all info at signup as members of 'users' collection in firestore db

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -4,7 +4,11 @@ import * as ImagePicker from 'expo-image-picker';
 import { StyleSheet, Text, View } from 'react-native';
 import styles from '../styles/styles.js';
 import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
-import { auth } from '../firebase.js';
+import { auth, db } from '../firebase.js';
+import { collection, addDoc } from "firebase/firestore";
+import * as FileSystem from 'expo-file-system';
+import mime from 'react-native-mime-types';
+
 
 
 const SignUpScreen = ({ navigation }) => {
@@ -14,49 +18,77 @@ const SignUpScreen = ({ navigation }) => {
   const [confirmPassword, setConfirmPassword] = useState('');
   const [image, setImage] = useState(null);
 
+const encodeImage = async (image) => {
+  try {
+    const fileExtension = image.uri.split('.').pop();
+    const mimeType = mime.lookup(fileExtension) || 'image/png';
+
+    // Read the image file as base64
+    const base64Image = await FileSystem.readAsStringAsync(image.uri, {
+      encoding: FileSystem.EncodingType.Base64,
+    });
+
+    const base64WithMimeType = `data:${mimeType};base64,${base64Image}`;
+
+    console.log('Base64-encoded image:', base64WithMimeType);
+    return base64WithMimeType;
+  } catch (error) {
+    console.error('Error encoding image to base64:', error);
+  }
+};
+
+
   const handleSignUp = () => {
     if (!name || !email || !password || !confirmPassword) {
       ToastAndroid.show('Please fill all fields.', ToastAndroid.SHORT);
       return;
     }
-
+  
     if (!validateEmail(email)) {
       ToastAndroid.show('Please enter a valid email address.', ToastAndroid.SHORT);
       return;
     }
-
+  
     if (password !== confirmPassword) {
       ToastAndroid.show('Passwords do not match.', ToastAndroid.SHORT);
       return;
     }
-
+  
     if (!image) {
       ToastAndroid.show('Please select a profile picture.', ToastAndroid.SHORT);
       return;
     }
-
-    createUserWithEmailAndPassword(auth, email, password).then((userCredential) => {
-      // Signed in 
-      const user = userCredential.user;
-
-      // Update the user profile
-      return updateProfile(user, {
-      }).then(() => {
-        // Profile updated!
-        // Clear the form
+  
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(async (userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+  
+        // Encode image to base64
+        const base64Image = await encodeImage(image);
+  
+        // Add user to Firestore
+        await addDoc(collection(db, 'users'), {
+          uid: user.uid,
+          name: name,
+          email: email,
+          image: base64Image
+        });
+  
         setName('');
         setEmail('');
         setPassword('');
         setConfirmPassword('');
         setImage(null);
-
+  
         ToastAndroid.show('User registered successfully.', ToastAndroid.SHORT);
-      });
-    })
+  
+        navigation.navigate('HomeScreen');
+      })
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
-        // Log the error or show it in a toast
+        
         console.log(errorCode, errorMessage);
         ToastAndroid.show(errorMessage, ToastAndroid.SHORT);
       });
@@ -77,11 +109,11 @@ const SignUpScreen = ({ navigation }) => {
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
       aspect: [1, 1],
-      quality: 1,
+      quality: 0.5,
     });
-
+  
     if (!result.canceled) {
-      setImage(result.assets[0].uri);
+      setImage({ uri: result.assets[0].uri, mimeType: result.assets[0].mimeType || 'image/png' });
     }
   };
 
@@ -96,7 +128,7 @@ const SignUpScreen = ({ navigation }) => {
       <View style={styles.outerContainer}>
         <TouchableOpacity onPress={handleSelectImage}>
           {image ? (
-            <Image style={styles.profileImage} source={{ uri: image }} />
+            <Image style={styles.profileImage} source={{ uri: image.uri }} />
           ) : (
             <View style={styles.addImage}>
               <Text style={{ color: '#87888c' }}>Add image</Text>
