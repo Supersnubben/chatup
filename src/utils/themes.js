da4e95a2a89ebf4cc13c2e22d1b6e188d6f7dc68
Robin Blondin <robin.blondin@protonmail.com>
2023-05-06 12:24:05 +0200

    Setup a theme style set.

---
+++ src/utils/themes.js
@@ -0,0 +1,33 @@
+const theme = {
+    colors: {
+      primary: '#9af3f3',
+      secondary: '#25b8bf',
+      thirdrary: '#044548',
+      textPrimary: 'white',
+      textSecondary: '#87888c',
+      inputBackground: '#EDFFFF',
+    },
+  
+    text: {
+      large: {
+        fontSize: 24,
+        fontWeight: 'bold',
+      },
+      medium: {
+        fontSize: 18,
+        fontWeight: 'bold',
+      },
+      small: {
+        fontSize: 14,
+        fontWeight: 'bold',
+      },
+    },
+  
+    spacing: {
+      small: 8,
+      medium: 12,
+      large: 16,
+    },
+  };
+  
+  export default theme;
