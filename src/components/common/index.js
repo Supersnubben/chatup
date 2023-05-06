6c6d484db35785d4366e209fdea12c5c507346f0
Robin Blondin <robin.blondin@protonmail.com>
2023-05-07 01:06:39 +0200

    Made a new file in common components folder.
    For easier and more clean import code.

---
+++ src/components/common/index.js
@@ -0,0 +1,5 @@
+export { default as CustomTextInput } from './CustomTextInput';
+export { default as CustomButton } from './CustomButton';
+export { default as BackButton } from './BackButton';
+export { default as LogoutButton } from './LogoutButton';
+export { default as AddButton } from './AddButton';
