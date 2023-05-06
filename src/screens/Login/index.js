f6f559e4dcbcf7f9998b29829ad7fbe456972aa9
Robin Blondin <robin.blondin@protonmail.com>
2023-05-06 16:30:52 +0200

    Moved code to new organzied structure.
    Updated code using new components and theme files.

---
+++ src/screens/Login/index.js
@@ -0,0 +1,77 @@
+import { ToastAndroid, Image, Text, TextInput, TouchableOpacity, View, KeyboardAvoidingView, Button } from 'react-native'
+import styles from './styles'
+import React, { useState } from 'react';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../firebase.js';
+
+const SignInScreen = ({ navigation }) => {
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleSignIn = () => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in successfully
+        const user = userCredential.user;
+        navigation.navigate('HomeScreen');
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // Show error message
+        if (errorCode === 'auth/user-not-found') {
+          ToastAndroid.show('User not found. Please register.', ToastAndroid.SHORT);
+        }
+        else if (errorCode === 'auth/wrong-password') {
+          ToastAndroid.show('Incorrect password. Please try again.', ToastAndroid.SHORT);
+        }
+        else if (errorCode === 'auth/invalid-email') {
+          ToastAndroid.show('Invalid email format. Please try again.', ToastAndroid.SHORT);
+        }
+        else {
+          ToastAndroid.show(errorMessage, ToastAndroid.SHORT);
+        }
+      });
+  };
+
+  const handlePress = () => {
+    navigation.navigate('SignUpScreen');
+  };
+
+
+  return (
+    <KeyboardAvoidingView
+      style={styles.shellContainer}
+      behavior='padding'>
+      <View style={styles.outerContainer}>
+        <Image
+          style={styles.logo}
+          source={require('/home/robin/repos/chatup/images/logo.png')} />
+        <View>
+          <Text style={styles.text}>Login to continue</Text>
+        </View>
+        <View style={styles.innerContainer}>
+          <TextInput
+            placeholder='Enter email adress'
+            placeholderTextColor={'#87888c'}
+            value={email}
+            onChangeText={text => setEmail(text)} />
+          <TextInput
+            placeholder='Enter password'
+            placeholderTextColor={'#87888c'}
+            secureTextEntry
+            value={password}
+            onChangeText={text => setPassword(text)} />
+          <Button title="Sign in"></Button>
+        </View>
+        <View>
+          <TouchableOpacity onPress={handlePress}>
+            <Text style={styles.text}>Create a new account</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </KeyboardAvoidingView>
+  )
+}
+
+export default SignInScreen
---
+++ src/screens/Login/styles.js
@@ -0,0 +1,38 @@
+import { StyleSheet } from 'react-native';
+import themes from '/home/robin/repos/chatup/src/utils/themes.js'
+
+export default StyleSheet.create({
+
+  outerContainer: {
+    flex: 1,
+    backgroundColor: themes.colors.primary,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+
+  text: {
+    color: themes.colors.textSecondary,
+    alignItems: 'center',
+    alignSelf: 'stretch',
+    marginTop: themes.spacing.medium,
+  },
+
+  logo: {
+    width: 300,
+    height: 191,
+  },
+
+  innerContainer: {
+    marginTop: themes.spacing.medium,
+    alignSelf: 'stretch',
+    gap: themes.spacing.small,
+  },
+
+  shellContainer: {
+    flex: 1,
+    backgroundColor: themes.colors.t
+    padding: 30,
+  },
+
+})
