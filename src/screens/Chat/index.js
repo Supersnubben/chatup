7770f660c63ca2097a16085ddfacf15a055aa52f
Robin Blondin <robin.blondin@protonmail.com>
2023-05-06 20:23:58 +0200

    Added boilerplate so navigator could find screens

---
+++ src/screens/Chat/index.js
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native'
+import React from 'react'
+import styles from './styles'
+
+const ChatScreen = () => {
+  return (
+    <View>
+      <Text>ChatScreen</Text>
+    </View>
+  )
+}
+
+export default ChatScreen
