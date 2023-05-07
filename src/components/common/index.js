6ce6da0cbaa1bda19fa2e15d87928fba0ad7b625
Robin Blondin <robin.blondin@protonmail.com>
2023-05-07 18:43:18 +0200

    Changed exports and added new buttons

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,5 +1,6 @@
 export { default as CustomTextInput } from './CustomTextInput';
 export { default as CustomButton } from './CustomButton';
-export { default as BackButton } from './BackButton';
+export { default as BackButtonSecondary } from './BackButtonSecondary';
+export { default as BackButtonPrimary } from './BackButtonPrimary';
 export { default as LogoutButton } from './LogoutButton';
 export { default as AddButton } from './AddButton';
