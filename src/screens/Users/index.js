faf283b5b6e542f57a6ee49f49670ddd77294def
Robin Blondin <robin.blondin@protonmail.com>
2023-05-07 20:36:51 +0200

    Fetched users and used UserTemplate to store data
    from users and displaying them one by one in a flatlist on UserScreen

--- src/screens/Users/index.js
+++ src/screens/Users/index.js
@@ -2,14 +2,31 @@ import { FlatList, TouchableOpacity, Text, View } from 'react-native'
 import styles from './styles'
 import React, { useState, useEffect } from 'react'
 import { BackButtonPrimary } from '../../components/common'
+import fetchUsers from '../../utils/fetchUsers'
+import UserTemplate from '../../components/UserTemplate'
 
 
 const UserScreen = ({ navigation }) => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const usersData = await fetchUsers();
+      const sortedUsers = Object.values(usersData).sort((a, b) =>
+        a.name.localeCompare(b.name),
+      );
+      setUsers(sortedUsers);
+    };
+
+    fetchData();
+  }, []);
   
 
 const handleBack = () => {
   navigation.navigate('HomeScreen');
 }
+
+const renderItem = ({ item }) => <UserTemplate user={item} />;
 
   return (
     <View style={styles.outerContainer}>
@@ -18,9 +35,10 @@ const handleBack = () => {
           <Text style={styles.text}>Select user</Text>
       </View>
       <View style={styles.innerContainer}>
-        <FlatList>
-
-        </FlatList>
+        <FlatList
+          data={users}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id} />
       </View>
       <View style={styles.footer}>
       </View>
