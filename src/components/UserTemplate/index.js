315c2c8878e8c065c46633aef48f2124ee6e40ea
Robin Blondin <robin.blondin@protonmail.com>
2023-05-07 18:51:50 +0200

    Changed name on components

---
+++ src/components/UserTemplate/index.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const UserTemplate = () => {
+  return (
+    <View>
+      <Text>UsersTemplate</Text>
+    </View>
+  )
+}
+
+export default UserTemplate
+
+const styles = StyleSheet.create({})
