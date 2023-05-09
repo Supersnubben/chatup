7ce480b207e25089bedfa94a94d2ceeb253edbca
Robin Blondin <robin.blondin@protonmail.com>
2023-05-09 20:28:19 +0200

    Created new component for sent messages

---
+++ src/components/chat/SentMessage/index.js
@@ -0,0 +1,15 @@
+import { Text, View } from 'react-native'
+import styles from './styles'
+import React from 'react'
+
+const index = () => {
+  return (
+    <View style={styles.container}>
+      <Text>index</Text>
+    </View>
+  )
+}
+
+export default index
+
+const styles = StyleSheet.create({})
