f5f4cb0509a1f30fcceff6bc8dd0e4270d72a486
Robin Blondin <robin.blondin@protonmail.com>
2023-05-09 17:46:42 +0200

    Added onPress function to the user component

--- src/components/UserTemplate/index.js
+++ src/components/UserTemplate/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
 import styles from './styles';
 
-const UserTemplate = ({ user }) => {
+const UserTemplate = ({ user, onPress }) => {
   return (
-    <View style={styles.container}>
+    <TouchableOpacity onPress={() => onPress(user)} style={styles.container}>
       <View style={styles.imageContainer}>
         <Image source={{ uri: user.image }} style={styles.profileImage} />
       </View>
       <Text style={styles.name}>{user.name}</Text>
-    </View>
+    </TouchableOpacity>
   );
 };
 
