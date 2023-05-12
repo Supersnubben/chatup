824bfec5856b4c02d575157f02f1bf4b63bedbeb
Robin Blondin <robin.blondin@protonmail.com>
2023-05-13 00:33:24 +0200

    Redesigned the styles of the sent component

--- src/components/chat/SentMessage/index.js
+++ src/components/chat/SentMessage/index.js
@@ -2,10 +2,10 @@ import { Text, View } from 'react-native'
 import styles from './styles'
 import React from 'react'
 
-const index = ({message}) => {
+const index = ({ message }) => {
   return (
-    <View style={styles.container}>
-      <Text>{message}</Text>
+    <View style={[styles.container, styles.align]}>
+      <Text style={styles.text}>{message}</Text>
     </View>
   )
 }
--- src/components/chat/SentMessage/styles.js
+++ src/components/chat/SentMessage/styles.js
@@ -7,10 +7,26 @@ const styles = StyleSheet.create({
     height: 'auto',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'rgba(0, 30, 255, 0.7)',
+    backgroundColor: 'rgba(0, 30, 255, 0.6)',
     padding: themes.spacing.large,
-    borderRadius: 20,
+    borderBottomLeftRadius: 15,
+    borderBottomRightRadius: 3,
+    borderTopLeftRadius: 15,
+    borderTopRightRadius: 15,
+
   },
+
+  align: {
+    alignSelf: 'flex-end',
+    marginRight: 10,
+    marginLeft: '25%',
+    marginBottom: 2,
+  },
+
+  text: {
+    color: themes.colors.textPrimary,
+  },
+
 });
 
 export default styles;
