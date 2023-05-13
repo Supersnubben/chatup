aecb3e2d365f71fd01bab9357395c25a1b9d600f
Robin Blondin <robin.blondin@protonmail.com>
2023-05-14 00:39:32 +0200

    Added datetime for the last message in template

--- src/components/chat/ConversationTemplate/index.js
+++ src/components/chat/ConversationTemplate/index.js
@@ -7,6 +7,16 @@ import { auth } from '../../../utils/firebase'
 const ConversationTemplate = ({ conversation }) => {
     const [otherUser, setOtherUser] = useState(null);
 
+    const lastMessageTimestamp = conversation.createdAt.toDate();
+    const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+    const day = days[lastMessageTimestamp.getDay()];
+    const hour = lastMessageTimestamp.getHours().toString().padStart(2, '0');
+    const minute = lastMessageTimestamp.getMinutes().toString().padStart(2, '0');
+
+    const formattedDate = `${day} ${hour}:${minute}`;
+
+
     useEffect(() => {
         const fetchOtherUser = async () => {
             const otherUserId = conversation.user1Id === auth.currentUser.uid
@@ -29,7 +39,7 @@ const ConversationTemplate = ({ conversation }) => {
                     <Text style={styles.nameText}>{otherUser.name}</Text>
                 </View>
                 <View>
-                    <Text style={styles.messageText}>{conversation.lastMessage} - { }</Text>
+                    <Text style={styles.messageText}>{conversation.lastMessage} - {formattedDate}</Text>
                 </View>
             </View>
 
