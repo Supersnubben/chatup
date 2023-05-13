0f5114e8d1616a90cec80324e4547c84791c126b
Robin Blondin <robin.blondin@protonmail.com>
2023-05-14 00:21:05 +0200

    Added functions to fetch and display conversations
    Conversations that includes current user in flatlist on the homescreen.

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,12 +1,42 @@
 import { FlatList, Image, TouchableOpacity, Text, View, BackHandler } from 'react-native'
 import styles from './styles'
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import { LogoutButton, AddButton } from '../../components/common'
 import fetchCurrentUser from '../../utils/fetchCurrentUser'
-import { auth } from '../../utils/firebase'
+import { getDocs, collection } from 'firebase/firestore';
+import { auth, db } from '../../utils/firebase'
+import ConversationTemplate from '../../components/chat/ConversationTemplate'
+import { useFocusEffect } from '@react-navigation/native'
 
 const HomeScreen = ({ navigation }) => {
   const [user, setUser] = useState('');
+  const [conversations, setConversations] = useState([]);
+
+  const fetchConversations = useCallback(async (userId) => {
+    // Fetch conversations for this user from Firebase
+    const querySnapshot = await getDocs(collection(db, "conversations"));
+    const conversationsData = [];
+    querySnapshot.forEach((doc) => {
+      if (doc.data().user1Id.includes(userId) || doc.data().user2Id.includes(userId)) {
+        conversationsData.push(doc.data());
+      }
+    });
+    setConversations(conversationsData);
+  }, []);
+
+  useEffect(() => {
+    fetchConversations(auth.currentUser.uid);
+  }, [fetchConversations]);
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchConversations(auth.currentUser.uid);
+    }, [fetchConversations])
+  );
+
+  const renderItem = ({ item }) => <ConversationTemplate conversation={item} />;
+
+
 
   useEffect(() => {
     const fetchData = async () => {
@@ -46,29 +76,30 @@ const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.outerContainer}>
       {user && (
-      <>
-      <View style={styles.header}>
-        <TouchableOpacity>
-          <Image 
-            style={styles.profileImage}
-            source={{uri: base64Image}}
-            resizeMode="cover" />
-        </TouchableOpacity>
-        <View style={styles.nameContainer}>
-          <Text style={styles.text}>{user.name}</Text>
-        </View>
-        <LogoutButton style={styles.logout} onPress={handleLogout} />
-      </View>
-      <View style={styles.innerContainer}>
-        <FlatList>
-
-        </FlatList>
-      </View>
-      <View style={styles.footer}>
-        <AddButton onPress={handleAddButton} />
-      </View>
-      </>
-    )}
+        <>
+          <View style={styles.header}>
+            <TouchableOpacity>
+              <Image
+                style={styles.profileImage}
+                source={{ uri: base64Image }}
+                resizeMode="cover" />
+            </TouchableOpacity>
+            <View style={styles.nameContainer}>
+              <Text style={styles.text}>{user.name}</Text>
+            </View>
+            <LogoutButton style={styles.logout} onPress={handleLogout} />
+          </View>
+          <View style={styles.innerContainer}>
+            <FlatList
+              data={conversations}
+              renderItem={renderItem}
+              keyExtractor={(item) => item.id} />
+          </View>
+          <View style={styles.footer}>
+            <AddButton onPress={handleAddButton} />
+          </View>
+        </>
+      )}
     </View>
   )
 }
