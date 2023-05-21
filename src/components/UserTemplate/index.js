e1b9bac90b166ef4d781887d999fc1ce13f56736
Robin Blondin <robin.blondin@protonmail.com>
2023-05-21 20:52:39 +0200

    Added statuscircle to component

--- src/components/UserTemplate/index.js
+++ src/components/UserTemplate/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import StatusCircle from '../StatusCircle';
 import { View, Text, Image, TouchableOpacity } from 'react-native';
 import styles from './styles';
 
@@ -7,6 +8,9 @@ const UserTemplate = ({ user, onPress }) => {
     <TouchableOpacity onPress={() => onPress(user)} style={styles.container}>
       <View style={styles.imageContainer}>
         <Image source={{ uri: user.image }} style={styles.profileImage} />
+        <View style={styles.statusCircleContainer}>
+          <StatusCircle active={user?.active} />
+        </View>
       </View>
       <Text style={styles.name}>{user.name}</Text>
     </TouchableOpacity>
--- src/components/UserTemplate/styles.js
+++ src/components/UserTemplate/styles.js
@@ -7,14 +7,29 @@ export default StyleSheet.create({
     marginLeft: 20,
     flexDirection: 'row',
     alignItems: 'center',
+    gap: 10,
   },
   imageContainer: {
-    marginRight: 10,
+    width: 50,
+    height: 50,
+    borderRadius: 25,
   },
+
   profileImage: {
     width: 50,
     height: 50,
     borderRadius: 25,
+    resizeMode: 'cover',
+  },
+
+  statusCircleContainer: {
+    position: 'absolute',
+    top: '80%',
+    right: 0,
+    transform: [{ translateY: -5 }],
+    width: 10,
+    height: 10,
+    borderRadius: 5,
   },
   name: {
     ...themes.text.small,
