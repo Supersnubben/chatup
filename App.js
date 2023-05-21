83409ef7f9fb57e977d7751ef13d37f81e86d7b3
Robin Blondin <robin.blondin@protonmail.com>
2023-05-21 19:29:40 +0200

    Added appstate to see if user is active in app

--- App.js
+++ App.js
@@ -1,9 +1,30 @@
-
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './src/navigation';
+import { AppState, AppStateIOS, Platform } from 'react-native';
 
 export default function App() {
+  const [appState, setAppState] = useState(AppState.currentState);
+
+  const handleAppStateChange = (nextAppState) => {
+    setAppState(nextAppState);
+  };
+
+  useEffect(() => {
+    AppState.addEventListener('change', handleAppStateChange);
+
+    if (Platform.OS === 'ios') {
+      AppStateIOS.addEventListener('change', handleAppStateChange);
+    }
+
+    return () => {
+      AppState.removeEventListener('change', handleAppStateChange);
+
+      if (Platform.OS === 'ios') {
+        AppStateIOS.removeEventListener('change', handleAppStateChange);
+      }
+    };
+  }, []);
   return (
     <NavigationContainer>
       <AppNavigator />
